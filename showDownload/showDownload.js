c74e33b690fe6f8813bd72b5101128e7dc7da510
februarysea <857254373@qq.com>
2018-10-26 09:38:36 +0800

    update showDownload

---
+++ showDownload/showDownload.js
@@ -0,0 +1,37 @@
+function showDownloadData() {
+    var inputBtn = document.getElementById("AliceSendBtn");
+    var inputAliceContext = "";
+    inputBtn.onclick = function() {
+            var Alice = "0xd5677cf67b5aa051bb40496e68ad359eb97cfbf8";
+            var AliceDownload = "math.pdf download list\nBob,Stan,Butters,Wendy.\n"
+            inputAliceContext = document.getElementById("AliceContext").value;
+            if(inputAliceContext === Alice){
+                document.getElementById("AliceText").innerText = AliceDownload;
+                document.getElementById("AliceContext").value = "";
+            }
+        }
+}
+
+
+function confirmAliceAddress(web3) {
+    var confirmBtn = document.getElementById("AliceConfirmBtn");
+    confirmBtn.onclick = function(){
+        var AliceAdd = document.getElementById("AliceAddress").value;
+        var code = web3.eth.getCode(AliceAdd, function(error, result) {
+            if(!error) {
+                if(result === "0x0") {
+                    alert("Address exists!");
+                    alert("Please input search address!");
+                    showDownloadData();
+                }
+                else {
+                    alert("No Address!");
+                }
+            }
+            else {
+                alert("Please input correct address!");
+            }
+        });
+    };
+}
+
