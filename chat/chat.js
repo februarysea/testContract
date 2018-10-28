3d6d0a4a331bb3a269fbc23029e8e9c45ac3f997
februarysea <857254373@qq.com>
2018-10-28 11:36:18 +0800

    finish testnet

--- chat/chat.js
+++ chat/chat.js
@@ -1,5 +1,6 @@
 var userName1;
 var userName2;
+var state = 0;
 
 function getInputAlice() {
     var inputBtn = document.getElementById("AliceSendBtn");
@@ -7,12 +8,12 @@ function getInputAlice() {
     inputBtn.onclick = function() {
         if(inputAliceContext === "") {
             inputAliceContext = document.getElementById("AliceContext").value;
-            document.getElementById("BobText").innerText = "[" + userName2 + "]:\n"+ inputAliceContext;
+            document.getElementById("BobText").innerText = "[" + userName1 + "]:\n"+ inputAliceContext;
             document.getElementById("AliceContext").value = "";
         }
         else {
             inputAliceContext = inputAliceContext+ "\n" + document.getElementById("AliceContext").value;
-            document.getElementById("BobText").innerText = "[" + userName2 + "]:\n"+ inputAliceContext;
+            document.getElementById("BobText").innerText = "[" + userName1 + "]:\n"+ inputAliceContext;
             document.getElementById("AliceContext").value = "";
         }
 
@@ -25,12 +26,12 @@ function getInputBob() {
     inputBtn.onclick = function() {
         if(inputBobContext === "") {
             inputBobContext = document.getElementById("BobContext").value;
-            document.getElementById("AliceText").innerText = "[" + userName1 + "]:\n"+  inputBobContext;
+            document.getElementById("AliceText").innerText = "[" + userName2 + "]:\n"+  inputBobContext;
             document.getElementById("BobContext").value = "";
         }
         else {
             inputBobContext = inputBobContext+ "\n" + document.getElementById("BobContext").value;
-            document.getElementById("AliceText").innerText = "[" + userName1 + "]\n"+  inputBobContext;
+            document.getElementById("AliceText").innerText = "[" + userName2 + "]\n"+  inputBobContext;
             document.getElementById("BobContext").value = "";
         }
 
@@ -46,8 +47,8 @@ function confirmAliceAddress(web3) {
                 if(!error) {
                     if(result === "0x0") {
                         alert("Address exists!");
-                        alert("Please input context!");
                         if (AliceAdd === "0x454ae50829FC65D3c5e013D6C097862A84FA7546"){
+                            $('#AliceConfirmBtn').prop('disabled', true);
                             getInputAlice();
                         }
                     }
@@ -72,8 +73,8 @@ function confirmBobAddress(web3) {
                 if(!error) {
                     if(result === "0x0") {
                         alert("Address exists!");
-                        alert("Please input context!");
-                        if(BobAdd == "0xfBbc3725fb8Ed8c8328800BE9b7a7D4F9fD07bA4"){
+                        if(BobAdd === "0xfBbc3725fb8Ed8c8328800BE9b7a7D4F9fD07bA4"){
+                            $('#BobConfirmBtn').prop('disabled', true);
                             getInputBob();
                         }
                     }
@@ -89,28 +90,48 @@ function confirmBobAddress(web3) {
     }
 }
 
-/*function getName() {
-    //userName1= prompt("Please input user1 name:");
-    //userName2 = prompt("Please input user2 name:");
-
-    document.getElementById("firstAlice").innerText = "\nUser1:" + userName1;
-    document.getElementById("firstBob").innerText = "\nUser2:" + userName2;
-    document.getElementById("secondAlice").innerText = "\nYour address is: 0xfBbc3725fb8Ed8c8328800BE9b7a7D4F9fD07bA4.";
-    document.getElementById("secondBob").innerText="\nYour address is: 0x454ae50829FC65D3c5e013D6C097862A84FA7546.";
-}*/
-
 function page1ToPage2() {
     page1ButtonA.onclick = function () {
-        userName1 = document.getElementById("userNameA").value;
-        alert("hello");
-        document.getElementById("page1A").hidden = true;
-        document.getElementById("page2A").hidden = false;
+        if(document.getElementById("userNameA").value !== "") {
+            userName1 = document.getElementById("userNameA").value;
+            document.getElementById("firstAlice").innerText = userName1;
+            document.getElementById("secondAlice").innerText = "Your address is: 0xfBbc3725fb8Ed8c8328800BE9b7a7D4F9fD07bA4.";
+            document.getElementById("img1").hidden = true;
+            document.getElementById("text1").hidden = true;
+            document.getElementById("input1").hidden = true;
+            document.getElementById("button1").hidden = true;
+            document.getElementById("showText1").hidden = false;
+            state++;
+            if(state === 2) {
+                document.getElementById("page1").hidden = true;
+                document.getElementById("page2").hidden = false;
+                document.getElementById("showText1").hidden = true;
+            }
+        }
+        else {
+            alert("Please input user1 name!");
+        }
     };
 
     page1ButtonB.onclick = function () {
-        userName2 = document.getElementById("userNameB").value;
-        alert("hello");
-        document.getElementById("page1B").hidden = true;
-        document.getElementById("page2B").hidden = false;
+        if(document.getElementById("userNameB").value !== "") {
+            userName2 = document.getElementById("userNameB").value;
+            document.getElementById("firstBob").innerText = userName2;
+            document.getElementById("secondBob").innerText = "Your address is: 0x454ae50829FC65D3c5e013D6C097862A84FA7546.";
+            document.getElementById("img2").hidden = true;
+            document.getElementById("text2").hidden = true;
+            document.getElementById("input2").hidden = true;
+            document.getElementById("button2").hidden = true;
+            document.getElementById("showText2").hidden = false;
+            state++;
+            if(state === 2) {
+                document.getElementById("page1").hidden = true;
+                document.getElementById("page2").hidden = false;
+                document.getElementById("showText1").hidden = true;
+            }
+        }
+        else {
+            alert("Please input user2 name!");
+        }
     };
 }
